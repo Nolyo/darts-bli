4e2d6b820b75a6bb7e6d4d993e5458787684cc68
Nolyo <yohann.jaffres@nextdecade.io>
2023-11-04 09:48:24 +0100

    Add new models and repositories

---
+++ repository/game/GameAsyncStorageRepository.ts
@@ -0,0 +1,21 @@
+import Repository from "./Repository";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+class AsyncStorageRepository implements Repository{
+    delete(item: any): void {
+    }
+
+    find(id: string): any {
+    }
+
+    async findAll(): Promise<any[]> {
+
+    }
+
+    save(item: any): void {
+    }
+
+    update(item: any): void {
+    }
+
+}
